78040e9e759561c1290d9e79ce7ec75d1ca50825
tiger423 <141384462+tiger423@users.noreply.github.com>
2025-09-13 20:15:24 +0800

    Update smbclient_setup.sh

--- smbclient_setup.sh
+++ smbclient_setup.sh
@@ -4,4 +4,5 @@ sudo apt install cifs-utils -y
 
 smbclient -L //172.18.3.23/share -N
  
-sudo mount -t cifs //172.18.3.23/share /home/ve/DVT/nas_storage   -o username=ve, password=ve ,vers=3.0, uid=$(id "-u" ve), gid=$(id "-g" ve)
+sudo mount -t cifs //172.18.3.23/share /home/ve/DVT/nas_share   -o username=ve, password=ve ,vers=3.0, uid=$(id "-u" ve), gid=$(id "-g" ve)
+
